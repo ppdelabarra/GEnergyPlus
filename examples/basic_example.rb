847fd27e072a85c2fa9a32d6212d2bc929ebb963
German Molina <germolinal@gmail.com>
2017-01-16 10:54:42 -0300

    .

--- examples/basic_example.rb
+++ examples/basic_example.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/rbplus' #this should become "require ''"
+require_relative '../lib/rbplus' #this should become "   require 'rbplus'   "
 
 model = EPlusModel.new("8.6.0")
 model.add("version",{"version identifier" => "8.6.0"})
